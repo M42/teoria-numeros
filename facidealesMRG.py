22f3ed4629d00339c74cc75d07868e78e1bb76ab
M42 <mromang08@gmail.com>
2017-05-10 12:34:50 +0200

    Factorización de ideales

--- facidealesMRG.py
+++ facidealesMRG.py
@@ -58,8 +58,7 @@ def LR(matriz):
 
 def normaIdeal(ideal, d):
     """
-    Calcula la norma de un ideal. Viene dado como lista de
-    generadores.
+    Calcula la norma de un ideal. Viene dado como lista de generadores.
     """
 
     # Reduce la matriz de relatores del ideal. La norma se obtiene
@@ -139,3 +138,63 @@ def producto(listaideales):
     generadores del ideal producto.
     """
     return reduce(productodos, listaideales, [(1,0),(0,1)])
+
+
+def factoriza_id(ideal, d):
+    """
+    Factoriza un ideal. La salida del algoritmo será una lista de
+    ideales que serán primos o unidades.
+    
+        * Si es el total, hemos terminado la descomposición.
+        * Para comprobar si es primo, calculamos la norma del ideal y
+            * si su norma es primo, es un ideal primo.
+            * si es el cuadrado de un primo, será primo sólo si el primo
+              no ramifica. Tenemos que estudiar si O/(p)O es un cuerpo o no.
+              Para ello calculamos el irreducible de e; p ramifica si y sólo
+              si f_p, el polinomio a módulo p, es irreducible.
+        * Si la norma del ideal no es primo, la factorizamos y tomamos uno
+          de sus primos para calcular sus divisores primos. Si no ramifica, 
+          su ideal generado es válido.
+        * Si ramifica, se descompone como dos ideales de norma p_i, que serán
+          precisamente <p_1,e-a> y <p_1,e-b> (se da sin demostración) para los
+          a,b en los que ha factorizado el polinomio irreducible en módulo p
+          como:  f_p = (X-a)(X-b).
+        * Para cada divisor encontrado, lo añadimos a la lista de factorización
+          y seguimos trabajando con el cociente.
+    
+    """
+    pass
+
+def esprimo(ideal,d):
+    """
+    Comprueba si un ideal dado es primo en un dominio O(d).
+
+    Dividimos por casos
+
+        * si I es el ideal total, sabemos que no es primo.
+        * si norma(I) es primo de Z, es primo.
+        * si norma(I) es cuadrado de primo. El ideal es primo si y sólo si
+          el p dado no ramifica. Un primo ramifica si y sólo si el polinomio
+          irreducible de e, Irr(e), es reducible; lo que se comprueba observando
+          si tiene raíces.
+        * en otro caso, no es primo.
+
+    """
+    pass
+
+
+def cocienteIdeal(I,J,d):
+    """
+    No sabemos calcular inversos de ideales en general, sólo sabemos calcular
+    inversos de ideales principales generados por un primo; así que factorizaremos
+    el ideal J para dividir por cada uno de sus factores a I.
+    """
+    pass
+
+def cocienteIdealPrincipalPrimo(I,p,d):
+    """
+    Para un ideal principal generado por un primo p. Podemos calcular su
+    inversa como 1/pI, dividiendo todos sus generadores; que además darán
+    números enteros.
+    """
+    pass
